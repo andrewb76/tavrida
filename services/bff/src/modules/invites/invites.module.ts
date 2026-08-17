24551c62baacbab1d87fbf2df1757a782f28cd4f
Andrew B <andrewb@bk.ru>
2026-08-17 20:36:16 +0300

    chore(invite): remove unused LogtoModule from InvitesModule

--- services/bff/src/modules/invites/invites.module.ts
+++ services/bff/src/modules/invites/invites.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ResolveRateLimitGuard } from '../../common/resolve-rate-limit.guard';
 import { AuthModule } from '../auth/auth.module';
-import { LogtoModule } from '../logto/logto.module';
 import { PlanConfigModule } from '../plan-config/plan-config.module';
 import { ScalarConfigModule } from '../scalar-config/scalar-config.module';
 import { UserProfileModule } from '../user-profile/user-profile.module';
@@ -9,7 +8,7 @@ import { InvitesController } from './invites.controller';
 import { InvitesService } from './invites.service';
 
 @Module({
-  imports: [AuthModule, LogtoModule, UserProfileModule, ScalarConfigModule, PlanConfigModule],
+  imports: [AuthModule, UserProfileModule, ScalarConfigModule, PlanConfigModule],
   controllers: [InvitesController],
   providers: [InvitesService, ResolveRateLimitGuard],
 })
